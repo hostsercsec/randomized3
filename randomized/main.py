054f4b31e976706b93a61b921770fbd8d6ad9616
hostsercsec <baber228standoff2@gmail.com>
2026-09-21 12:17:33 +0300

    Add files via upload

--- randomized/main.py
+++ randomized/main.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Dispatcher, Bot
+from aiogram import Bot, Dispatcher
 from handler import router
 
 bot = Bot(token="8407452828:AAElvoYfmhGeTdHXlbK1RcK8ZQ0KrAhWCZI")
@@ -8,8 +8,14 @@ dp = Dispatcher()
 dp.include_router(router)
 
 async def main():
+    print("1. main запущен")
+
     await bot.delete_webhook(drop_pending_updates=True)
+    print("2. webhook удалён")
+
+    print("3. запускаю polling")
     await dp.start_polling(bot)
 
-if __name__ == "main":
+if name == "main":
+    print("4. запускаю asyncio")
     asyncio.run(main())
